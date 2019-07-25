610f7a11af22a21c21beb4199bce967421719322
liunig <13265775433@163.com>
2019-07-25 10:21:30 +0800

    review radix sort

--- src/main/java/algorithm/sort/RadixSort.java
+++ src/main/java/algorithm/sort/RadixSort.java
@@ -1,6 +1,10 @@
 package algorithm.sort;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -9,10 +13,57 @@ import java.util.Random;
  */
 public class RadixSort {
     public static void main(String[] args) {
-
+        int arr[] = {53, 3, 542, 748, 14, 214};
+        // 80000000 * 11 * 4 / 1024 / 1024 / 1024 =3.3G
+//		int[] arr = new int[800000];
+//		for (int i = 0; i < 800000; i++) {
+//            // 生成一个[0, 8000000) 数
+//			arr[i] = (int) (Math.random() * 800000);
+//		}
+//        System.out.println(now);;
+//        Date data1 = new Date();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String date1Str = simpleDateFormat.format(data1);
+//        System.out.println("排序前的时间是=" + date1Str);
+        System.out.println(Arrays.toString(arr));
+        sort(arr);
+//        Date data2 = new Date();
+//        String date2Str = simpleDateFormat.format(data2);
+//        System.out.println("排序前的时间是=" + date2Str);
+        System.out.println(Arrays.toString(arr));
     }
-    public static void sort(Comparable[] arr) {
 
+    public static void sort(int[] arr) {
+        int length = arr.length;
+        int max = arr[0];
+        for (int i = 1; i < length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        int maxLength = (max + "").length();
+        int[][] bucket = new int[10][length];
+
+        int[] bucketElementCounts = new int[10];
+
+        for (int i = 0, n = 1; i < maxLength; i++, n *= 10) {
+            for (int j = 0; j < length; j++) {
+                int digitOfElement = arr[j] / n % 10;
+                bucket[digitOfElement][bucketElementCounts[digitOfElement]] = arr[j];
+                bucketElementCounts[digitOfElement]++;
+            }
+
+            int index = 0;
+            for (int j = 0; j < bucketElementCounts.length; j++) {
+                if (bucketElementCounts[j] != 0) {
+                    for (int k = 0; k < bucketElementCounts[j]; k++) {
+                        arr[index++] = bucket[j][k];
+                    }
+                }
+                bucketElementCounts[j] = 0;
+            }
+        }
     }
 
     private static boolean less(Comparable v, Comparable w) {
